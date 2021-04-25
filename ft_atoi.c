29f8f91fb4001039053f5dcd99bb541bf3bf0e16
Andrey Lazarev <lazarev@MacBook-Air-Andrey.local>
2021-04-25 17:58:31 +0300

    fix ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -12,6 +12,13 @@
 
 #include "libft.h"
 
+static int     ft_isspace(int c)
+{
+        if (c == ' ' || (c >= 9 && c <= 13))
+                return (1);
+        return (0);
+}
+
 int	ft_atoi(const char *nptr)
 {
 	unsigned int	i;
